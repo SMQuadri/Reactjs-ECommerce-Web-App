f09130bc10bf2e7bfe8f7850384177713eb223ce
Syed Mohammad Quadri <55687431+SMQuadri@users.noreply.github.com>
2020-08-26 21:04:41 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from 'firebase';
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyDV76Redi9y4ojJqsmDTfceLMz7FJWF3a0",
-    authDomain: "nike-10001.firebaseapp.com",
-    databaseURL: "https://nike-10001.firebaseio.com",
-    projectId: "nike-10001",
-    storageBucket: "nike-10001.appspot.com",
-    messagingSenderId: "322666058500",
-    appId: "1:322666058500:web:4de48bf31614a56deab651",
-    measurementId: "G-4YWTPD6XRV"
+    // Paste your Firebase config snippet here, as guided in the steps in README.md
 });
 
 const auth = firebase.auth();
